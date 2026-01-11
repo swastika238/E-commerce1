76246354a73e498f62acd022df8c5b9d46e7e82b
Swastika <swastikadhakal37@gmail.com>
2026-01-11 19:02:43 +0545

    style:add tailwind styling to login  page

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -14,16 +14,18 @@ const Login = () => {
         setSuccess("");
     }
   return (
-    <div>
-      <h2>
+    <div className='max-w-md mx-auto mr-12 p-6 bg-white rounded-lg shadpw-md'>
+      <h2 className='text-lg font-bold mb-6 text-center '>
             Login
       </h2>
-      <form>
+      <form onSubmit={handleLogin}className='space-y-4'> 
         <input type="email"placeholder='Email'value={email} onChange={(e)=>setEmail(e.targetvalue)}/>
         <imput type=" password"placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
-            <button type="submit">Login</button>
+         <button className="w-full bg-blue-500 text-white py-2 rounded-md">
+          Login
+        </button>   
       </form>
-      {error && <p>{error}</p>}
+      {error && <p className='mt-3.5 text-gray-500'>{error}</p>}
       {success && <p>{success}</p>}
     </div>
   )

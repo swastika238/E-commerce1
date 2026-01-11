e19d85bcb157cd1c5bf0daa560c9f7459c373b1f
Swastika <swastikadhakal37@gmail.com>
2026-01-11 18:34:06 +0545

    Add Login page

---
+++ src/page/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+import { getAuth } from 'firebase/auth'
+
+const Login = () => {
+    const[email,setEmail]=useState("");
+    const[password,setPassword]=("");
+    const[error,setError]=useState("");
+    const[success,setSuccess]=useState("")
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login

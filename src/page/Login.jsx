96bb7125afd0aba6f87ac5f04d3f36ebb3bbcee5
Swastika <swastikadhakal37@gmail.com>
2026-01-11 18:48:20 +0545

    refractor:restructure Login file

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import { getAuth } from 'firebase/auth'
 
 const Login = () => {
@@ -7,6 +8,11 @@ const Login = () => {
     const[password,setPassword]=("");
     const[error,setError]=useState("");
     const[success,setSuccess]=useState("")
+    const handleLogin=async(e)=>{
+        e.preventDefault("");
+        setError("");
+        setSuccess("");
+    }
   return (
     <div>
       
